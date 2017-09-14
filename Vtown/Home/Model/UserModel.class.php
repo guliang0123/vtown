01fe8b6b2bedf1d2199766956bdb7175c58b75d4
guliang0123 <898148191@qq.com>
2017-09-14 10:46:32 +0800

    修改验证规则

--- Vtown/Home/Model/UserModel.class.php
+++ Vtown/Home/Model/UserModel.class.php
@@ -5,11 +5,11 @@ use Think\Model;
 
 class UserModel extends Model
 {
-	  // 自动验证
+    // 自动验证
     protected $_validate = array(
-        array('username','/\w{5,12}/','用户名格式不正确',0,'regex',1), 
+        array('username','/\w{6,12}/','用户名格式不正确',0,'regex',1), 
         array('username','','用户名已经存在！',0,'unique',1), 
-        array('pass','/\w{6,12}/','密码格式不正确！',0,'regex',1),
+        array('pass','/\w{8,12}/','密码格式不正确！',0,'regex',1),
         array('repass','pass','密码不一致',0,'confirm'), 
   	    array('phone','','手机号已存在','0','unique',1)
      );
